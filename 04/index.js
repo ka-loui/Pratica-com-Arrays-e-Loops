b5276d89a9bd3930f186b38caddee506ab886978
Karen Almeida <134981964+ka-loui@users.noreply.github.com>
2023-07-13 11:36:17 -0300

    Update index.js
    Resolvido no Hackerhank

---
+++ 04/index.js
@@ -0,0 +1,17 @@
+function solucao(sequencia) {
+	//seu codigo aqui
+let indice = 0
+for (let item of sequencia) {
+    if (item === ">"){
+        indice++
+    } else {
+        indice--
+    }
+    if (indice < 0){
+        indice = 6
+    } else if (indice > 6){
+        indice = 0
+    }
+}
+    console.log(indice)
+}
