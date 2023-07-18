e5e7624e29360e73037d0886a4dd6d986873a496
Karen Almeida <134981964+ka-loui@users.noreply.github.com>
2023-07-18 18:36:27 -0300

    Exercício 10 resolvido

---
+++ 10/index.js
@@ -0,0 +1,11 @@
+let pontos = 0
+
+for (let i = 0; i < resultados.length; i++) {
+    if (resultados[i] === "V") {
+        pontos += 3;
+    } else if (resultados[i] === "E") {
+        pontos += 1;
+    }
+}
+
+console.log(pontos)
